3ac346a588acc1bb50f2b707e32034d9a7ea026f
Hind <hindsamiri2@gmail.com>
2022-02-02 14:33:14 +0100

    modificaciones

--- src/main/java/Ejercicios/Ej6.java
+++ src/main/java/Ejercicios/Ej6.java
@@ -15,12 +15,13 @@ import java.util.Scanner;
  * @author hinda
  */
 public class Ej6 {
+
     /*Hacer un programa que determine si dos arrays son iguales.
 Probarlo con los arrays [1,2,3] y [1,2,3], [1,2,3,4] y [1,2,3] , [1,2,3] y [1,2,2]. 
 Este ejercicio hay que hacerlo de dos formas: la primera, usando la clase Arrays; 
 la segunda, creando un método que vaya comparando los valores de los arrays y devuelva true si todos son iguales o false en caso contrario.*/
-   //metodos
-     public static int LeerPorTeclado() {
+    //metodos
+    public static int LeerPorTeclado() {
         Scanner sc = new Scanner(System.in);
         boolean solicitarDatos = true;
         int numero = 0;
@@ -40,41 +41,57 @@ la segunda, creando un método que vaya comparando los valores de los arrays y d
         } while (numero <= 0 || solicitarDatos);
         return numero;
     }
-     
-    public static int enteroRango(int MIN_VALUE,int MAX_VALUE){
+
+    public static int enteroRango(int MIN_VALUE, int MAX_VALUE) {
         Random rd = new Random();
         int numAleatorio = 0;
-        if(MIN_VALUE>0 &&  MAX_VALUE>0){
-            numAleatorio=rd.nextInt(MAX_VALUE-MIN_VALUE+1)+MIN_VALUE;
+        if (MIN_VALUE > 0 && MAX_VALUE > 0) {
+            numAleatorio = rd.nextInt(MAX_VALUE - MIN_VALUE + 1) + MIN_VALUE;
         }
         return numAleatorio;
     }
-     
-    public static void main(String[] args) {
-        int lentghArray1=LeerPorTeclado();
-        int lentghArray2=LeerPorTeclado();
-        
-        int[] array1=new int[lentghArray1];
-        int[] array2=new int[lentghArray2];
-        
-        //incialize arrays con aleatorio entre rango
-        for(int i=0;i<array1.length;i++){
-            array1[i]=enteroRango(1,10);
+
+    public static boolean comprareArrays(int[] array1, int[] array2) {
+        //boolean egual = true;
+        if (array1.length == array2.length) {
+            for (int i = 0; i < array1.length; i++) {
+                if (array1[i] != (array2[i])) {
+                    return false;
+                }
+            }
+            return true;
         }
-        
-        for(int i=0;i<array2.length;i++){
-            array2[i]=enteroRango(1,10);
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int lentghArray1 = LeerPorTeclado();
+        int lentghArray2 = LeerPorTeclado();
+
+        int[] array1 = new int[lentghArray1];
+        int[] array2 = new int[lentghArray2];
+
+        //incialize arrays con aleatorio entre rango
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = enteroRango(1, 10);
+        }
+
+        for (int i = 0; i < array2.length; i++) {
+            array2[i] = enteroRango(1, 10);
         }
         //mostrar los dos arrays
         System.out.println(Arrays.toString(array1));
         System.out.println(Arrays.toString(array2));
-        
+
         //compara los dos listas usando classe Arrays
-        if(Arrays.equals(array1, array2)){
+        if (Arrays.equals(array1, array2)) {
             System.out.println("Arrays eguales");
-        }else{
+        } else {
             System.out.println("No son eguales");
         }
+        
+        //comparar con el metodo
+        comprareArrays(array1,array2);
     }
 
 }
