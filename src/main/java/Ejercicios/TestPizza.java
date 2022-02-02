a4336e2244c387cee6b366a263f929fc4713c0e1
Hind <hindsamiri2@gmail.com>
2022-02-02 14:33:55 +0100

    Classe Pizza

---
+++ src/main/java/Ejercicios/TestPizza.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicios;
+
+/**
+ *
+ * @author hinda
+ */
+public class TestPizza {
+    public static void main(String[] args) {
+        Pizza bbq=Pizza.BARBACOA;
+        
+        switch(bbq){
+            case BARBACOA:
+                break;
+            case CARBONARA:
+                    break;
+        }
+        
+      
+    }
+}
