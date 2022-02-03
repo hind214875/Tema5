c840887852d3fe956b5c2d17f73c7460386d5495
Hind <hindsamiri2@gmail.com>
2022-02-03 12:41:26 +0100

    arrayList

---
+++ src/main/java/arrayList/Ej22.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arrayList;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author hinda
+ */
+public class Ej22 {
+    public static void main(String[] args) {
+        //crear miLista de typo ListaInteger(la classe)
+        ListaInteger miLista=new ListaInteger(10);
+        
+        ArrayList<Integer> list =miLista.getLista();
+        
+    }
+    
+}
