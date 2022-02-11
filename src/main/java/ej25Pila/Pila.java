da62a2dd36a0ed9b8540583b53397151705210cd
hind <hindsamiri2@gmail.com>
2022-02-11 11:36:52 +0100

    terminar

--- src/main/java/ej25Pila/Pila.java
+++ src/main/java/ej25Pila/Pila.java
@@ -14,23 +14,23 @@ public class Pila {
 
     //atributo
     private ArrayList<Character> pila;
+    private int tamanoPila;
 
     //constrector 
-    public Pila() {
+    public Pila(int tamanoPila) {
         this.pila = new ArrayList<>();
+        this.tamanoPila = tamanoPila;
+
     }
 
     //Saber si la pila está vacía.
     public boolean estaVacia() {
-        if (pila.isEmpty()) {
-            return true;
-        }
-        return false;
+        return pila.isEmpty();
     }
 
     //apilar elemento (en inglés push), lo inserta donde proceda.
     public boolean push(char c) {
-        if (pila.size() < 5) {
+        if (pila.size() < tamanoPila) {
             pila.add(c);
             return true;
         }
@@ -40,9 +40,10 @@ public class Pila {
     //desapilar (en inglés pop), que saca el elemento que toque.
     public char pop() {
         if (!(pila.isEmpty())) {
-            if (pila.size() <= 5) {
-                int posicion = pila.indexOf(pila.size());
-                return pila.get(posicion);
+            if (pila.size() <= tamanoPila) {
+                char p = pila.get(pila.size() - 1);
+                pila.remove(pila.size() - 1);
+                return p;
             }
 
         }
@@ -69,8 +70,29 @@ public class Pila {
         pila.forEach(System.out::println);
     }
 
+    public void VaciarPila() {
+        pila.clear();
+    }
+
     //rellenar (char[] array), que recibe un array de letras y almacena en la pila los char que hay en el array. 
     //La pila se ajusta su tamaño según el número de elementos que tenga el array.
+    public void rellenarArrayChar(char[] letras) {
+        for (int i = 0; i < letras.length; i++) {
+            char c = letras[i];
+            pila.add(c);
+        }
+    }
     //char[] sacarElementos(), saca los elementos de la pila y los devuelva en un array de char. 
     //La pila queda vacía.
+
+    public char[] sacarElementos() {
+        char[] arrayChar = new char[pila.size()];
+        
+        for (int i = 0; i < pila.size(); i++) {
+            if (!(pila.isEmpty())) {
+                arrayChar[i] = pila.get(i);      
+            }
+        }
+         return arrayChar;   
+    }
 }
